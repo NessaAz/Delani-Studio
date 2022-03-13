f2e936f41c43cadf8f0a93b9f332577500978118
NessaAz <vanessa.azenwa@gmail.com>
2022-03-13 23:40:45 +0300

    add js click actions in what we do cards and hover effects on portfolio container

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+$(".about-btn").mouseenter(function () {
+	$(this).css("animation-play-state", "paused");
+});
+$(".about-btn").mouseleave(function () {
+	$(this).css("animation-play-state", "running");
+});
+
+$(document).ready(function () {
+	toggleCards();
+	addHoverEffect();
+});
+
 const toggleCards = () => {
 	$(".card-body p").hide();
 	$("#whatWeDo .card").click(function () {
 		const pElement = $(this).find("p");
 		pElement.slideToggle();
 		$(this).toggleClass("bg-secondary");
+	});
+};
+
+const addHoverEffect = () => {
+	const overlay = $("#portfolio h5");
+	overlay.hide();
+	$(".portfolio-container").hover(function () {
+		$(this).find("h5").slideToggle("fast");
 	});
 };
